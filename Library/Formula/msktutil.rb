c4517158f6bc1da963e3bc3d97225eceaff03978
Claudio Luck <cluck@ini.uzh.ch>
2015-08-28 20:07:39 +0200

    msktutil: 1.0rc1, now from SF.net

--- Library/Formula/msktutil.rb
+++ Library/Formula/msktutil.rb
@@ -1,14 +1,11 @@
 class Msktutil < Formula
   desc "Program for interoperability with Active Directory"
   homepage "https://code.google.com/p/msktutil/"
-  url "https://msktutil.googlecode.com/files/msktutil-0.5.1.tar.bz2"
-  sha256 "ec02f7f19aa5600c5d20f327beaef88ee70211841dc01fa42eb258ae840ae6f0"
+  url "http://downloads.sourceforge.net/project/msktutil/msktutil-1.0rc1.tar.bz2"
+  sha256 "61ff3180f576bfc4e6693b5fb47bd6be2227cd81b3514e6c8a0f9134ebd81bed"
 
   def install
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}",
+    system "./configure", "--prefix=#{prefix}",
                           "--mandir=#{man}"
     system "make", "install"
   end
